ffd793915deeee45519ead5dd67fbb80c4f08899
Lucas Simões <lucasbritosimoes@hotmail.com>
2024-08-10 10:39:51 -0300

    Correções

--- Aula01Aula10/Aula03/aula03.cs
+++ Aula01Aula10/Aula03/aula03.cs
@@ -6,11 +6,11 @@ class aula03{
         byte n1 = 10; //pode armazenar entre 0 e 255
         int num = 0;
         char letra = 'c'; //coisas simples não palavras
-        float valor = '5.0f'; //F no final para o padrão de float
+        float valor = 5.03f ; //F no final para o padrão de float
         string nome = "Nome"; //Aspas duplas
 
         var aux = nome; 
         //Quando eu uso operador VAR é basicamente eu não especificar o tipo da variavel, então ela vai ser definida com o que ele receber posteriormente
-        Console.WriteLine(aux);
+        Console.WriteLine("Seu nome é" + aux);
     }
 }   
