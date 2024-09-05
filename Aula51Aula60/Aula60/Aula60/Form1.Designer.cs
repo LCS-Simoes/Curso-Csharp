22356f9dac2bbe8d90a84447859dccd2d3bed766
Lucas Simões <lucasbritosimoes@hotmail.com>
2024-09-05 17:12:46 -0300

    Event click

--- Aula51Aula60/Aula60/Aula60/Form1.Designer.cs
+++ Aula51Aula60/Aula60/Aula60/Form1.Designer.cs
@@ -1,6 +1,6 @@
 ﻿namespace Aula60
 {
-    partial class Form1
+    partial class F_Principal
     {
         /// <summary>
         /// Variável de designer necessária.
@@ -28,51 +28,51 @@
         /// </summary>
         private void InitializeComponent()
         {
-            this.btn_teste = new System.Windows.Forms.Button();
+            this.btn_ok = new System.Windows.Forms.Button();
             this.lb_link = new System.Windows.Forms.Label();
-            this.tb_nome = new System.Windows.Forms.TextBox();
+            this.tb_texto = new System.Windows.Forms.TextBox();
             this.SuspendLayout();
             // 
-            // btn_teste
+            // btn_ok
             // 
-            this.btn_teste.BackColor = System.Drawing.Color.Black;
-            this.btn_teste.Font = new System.Drawing.Font("Nirmala UI", 15.75F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
-            this.btn_teste.ForeColor = System.Drawing.Color.Red;
-            this.btn_teste.Location = new System.Drawing.Point(24, 60);
-            this.btn_teste.Name = "btn_teste";
-            this.btn_teste.Size = new System.Drawing.Size(213, 80);
-            this.btn_teste.TabIndex = 0;
-            this.btn_teste.Text = "Botão";
-            this.btn_teste.UseVisualStyleBackColor = false;
-            this.btn_teste.Click += new System.EventHandler(this.button1_Click);
+            this.btn_ok.BackColor = System.Drawing.Color.Black;
+            this.btn_ok.Font = new System.Drawing.Font("Nirmala UI", 15.75F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.btn_ok.ForeColor = System.Drawing.Color.Red;
+            this.btn_ok.Location = new System.Drawing.Point(24, 60);
+            this.btn_ok.Name = "btn_ok";
+            this.btn_ok.Size = new System.Drawing.Size(213, 80);
+            this.btn_ok.TabIndex = 0;
+            this.btn_ok.Text = "OK";
+            this.btn_ok.UseVisualStyleBackColor = false;
+            this.btn_ok.Click += new System.EventHandler(this.btn_ok_Click);
             // 
             // lb_link
             // 
             this.lb_link.AutoSize = true;
             this.lb_link.Font = new System.Drawing.Font("Microsoft Sans Serif", 12F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
-            this.lb_link.Location = new System.Drawing.Point(20, 28);
+            this.lb_link.Location = new System.Drawing.Point(20, 158);
             this.lb_link.Name = "lb_link";
             this.lb_link.Size = new System.Drawing.Size(130, 20);
             this.lb_link.TabIndex = 1;
             this.lb_link.Text = "lucassimoes.com";
             this.lb_link.Click += new System.EventHandler(this.label1_Click);
             // 
-            // tb_nome
+            // tb_texto
             // 
-            this.tb_nome.Location = new System.Drawing.Point(24, 158);
-            this.tb_nome.Name = "tb_nome";
-            this.tb_nome.Size = new System.Drawing.Size(213, 20);
-            this.tb_nome.TabIndex = 2;
+            this.tb_texto.Location = new System.Drawing.Point(24, 34);
+            this.tb_texto.Name = "tb_texto";
+            this.tb_texto.Size = new System.Drawing.Size(213, 20);
+            this.tb_texto.TabIndex = 2;
             // 
-            // Form1
+            // F_Principal
             // 
             this.AutoScaleDimensions = new System.Drawing.SizeF(6F, 13F);
             this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
             this.ClientSize = new System.Drawing.Size(1173, 752);
-            this.Controls.Add(this.tb_nome);
+            this.Controls.Add(this.tb_texto);
             this.Controls.Add(this.lb_link);
-            this.Controls.Add(this.btn_teste);
-            this.Name = "Form1";
+            this.Controls.Add(this.btn_ok);
+            this.Name = "F_Principal";
             this.Text = "Curso C#";
             this.Load += new System.EventHandler(this.Form1_Load);
             this.ResumeLayout(false);
@@ -82,9 +82,9 @@
 
         #endregion
 
-        private System.Windows.Forms.Button btn_teste;
+        private System.Windows.Forms.Button btn_ok;
         private System.Windows.Forms.Label lb_link;
-        private System.Windows.Forms.TextBox tb_nome;
+        private System.Windows.Forms.TextBox tb_texto;
     }
 }
 
--- Aula51Aula60/Aula60/Aula60/Form1.cs
+++ Aula51Aula60/Aula60/Aula60/Form1.cs
@@ -10,16 +10,25 @@ using System.Windows.Forms;
 
 namespace Aula60
 {
-    public partial class Form1 : Form
+    public partial class F_Principal : Form
     {
-        public Form1()
+        public F_Principal()  
         {
             InitializeComponent();
         }
-
-        private void button1_Click(object sender, EventArgs e)
+        private void btn_ok_Click(object sender, EventArgs e)
         {
+            //Versão resumida
 
+            lb_link.Text = tb_texto.Text;
+
+            /*
+            Versão compelxa
+
+                string txt_Modify
+                Modify = tb_texto.Text; //.Text é para eu pegar o texto dentro do textbox
+                ink.Text = txt_Modify; //.Text recebe a variavel txt que havia pegado o texto do text box
+            */
         }
 
         private void label1_Click(object sender, EventArgs e)
@@ -31,5 +40,6 @@ namespace Aula60
         {
 
         }
+
     }
 }
